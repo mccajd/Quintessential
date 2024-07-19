43a6f3b86047b19769112439a54ad51aa7a263ec
Rural Young <ruralyoungiii@gmail.com>
2024-07-19 00:18:07 -0400

    Modified BeaconNode to 'hold' an item and also showcase what item it is 'holding' Also created a little seperate level for testing

--- Scripts/BeaconNode.gd
+++ Scripts/BeaconNode.gd
@@ -4,7 +4,7 @@ var id
 var inputs: Array = []
 var outputs = []
 var selectedTransformation
-
+var slottedItem: Item
 signal selected
 
 # Called when the node enters the scene tree for the first time.
@@ -18,3 +18,15 @@ func _process(delta):
 		$AnimatedSprite2D.play("filled")
 		return
 	$AnimatedSprite2D.play("empty")
+
+func add_item(item: Item):
+	if slottedItem == null:
+		slottedItem = item
+		$SlottedItemPreview.texture = item.sprite
+		return
+
+func remove_item():
+	if slottedItem:
+		slottedItem = null
+		$SlottedItemPreview.texture = null
+		return
