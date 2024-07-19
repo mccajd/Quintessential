400404f35ff0681690fe763ba111ed87b833ca6d
Rural Young <ruralyoungiii@gmail.com>
2024-07-19 00:16:45 -0400

    Added an Item class and an ItemsDB dictionary, created a gold and silver resource for testing

---
+++ Scripts/Items.gd
@@ -0,0 +1,6 @@
+class_name Items
+
+const itemDB = {
+	'gold' : { 'name' : 'Gold', 'sprite' : 'res://assets/items/gold.png', 'ID' : 0 },
+	'silver' : { 'name' : 'Silver', 'sprite' : 'res://assets/items/silver.png', 'ID' : 1 }
+}
