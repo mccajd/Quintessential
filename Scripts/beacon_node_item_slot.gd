83917117c212da33247a6d1cddd0fa403f5b7ed5
Jacob McCann <mccajd@gmail.com>
2024-07-21 15:42:24 -0400

    Generic item slot, add inputs

--- Scripts/beacon_node_item_slot.gd
+++ Scripts/beacon_node_item_slot.gd
@@ -1,20 +1,20 @@
 extends Area2D
 
-@export var output_item: OutputItem
-var node_destination_options: Array[int]
-var selected_node_destination: int
+var item_key
+#var node_destination_options: Array[int]
+#var selected_node_destination: int
 
 func _ready():
 	$BackgroundTextureRect.texture = load("res://assets/beacon/node-bg-slot.png")
 
 
 func _process(delta):
-	if output_item == null:
+	if item_key == null:
 		$ItemTextureRect.texture = load("res://assets/beacon/empty.png")
 		return
 	
-	var item = Items.itemDB[output_item.item_key]
+	var item = Items.itemDB[item_key]
 	$ItemTextureRect.texture = load(item.sprite)
 
-func set_output_item(item: OutputItem):
-	output_item = item
+func set_item(key):
+	item_key = key
