1283c36b98b8cba2a87ee72a1f5cd5131b45c312
Jacob McCann <mccajd@gmail.com>
2024-07-21 17:02:17 -0400

    clear an unused prop

--- Scripts/output_item.gd
+++ Scripts/output_item.gd
@@ -6,7 +6,7 @@ class_name OutputItem
 # i swear if i find out that int is the one exception to the "always nullable"
 # obsession that godot seems to have, i'm gonna blow a gasket
 # update: BAHAHAHAHA HAHAHAHAH AAAHAHAHAH FUCK
-@export var destination_node: int
+#@export var destination_node: int
 # can use this to set an additional option (e.g., if its a metal ascension and 
 # we need the player to choose a specific metal)
 @export var additional_transformation: String
