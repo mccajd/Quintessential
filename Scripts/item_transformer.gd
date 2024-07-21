a43909a39f4bcea46176fc12a8e93ba3e81f311c
Jacob McCann <mccajd@gmail.com>
2024-07-21 13:24:39 -0400

    Extract method on array compare

--- Scripts/item_transformer.gd
+++ Scripts/item_transformer.gd
@@ -17,18 +17,8 @@ func _transform_solution(item_keys: Array[String]):
 
 	
 func find_match(recipe, item_keys: Array):
-	# reference types OMEGALUL
-	var deep_copy = item_keys.duplicate()
-	
-	for ingredient in recipe.required_ingredients:
-		var ix = deep_copy.find(ingredient)
-		if ix > -1:
-			deep_copy.remove_at(ix)
-		else:
-			return false
-
 	# if any ingredients haven't been used, treat as a failure condition
 	# prevents brute forcing puzzles by adding everything as an input at once
 	# NOTE.jmc - if the game proves too obtuse, perhaps introduce this as either
 	# 		a difficulty option or a consequence of the shadows in late game
-	return deep_copy.size() == 0
+	return Helpers.compare_arrays(recipe.required_ingredients, item_keys)
