aba987500364d36d84c986134488b56ddd9060ac
Jacob McCann <mccajd@gmail.com>
2024-07-20 21:04:19 -0400

    add more items, fix hover not working

--- Scripts/Items.gd
+++ Scripts/Items.gd
@@ -2,5 +2,13 @@ class_name Items
 
 const itemDB = {
 	'gold' : { 'name' : 'Gold', 'sprite' : 'res://assets/items/gold.png', 'id' : 0 },
-	'silver' : { 'name' : 'Silver', 'sprite' : 'res://assets/items/silver.png', 'id' : 1 }
+	'silver' : { 'name' : 'Silver', 'sprite' : 'res://assets/items/silver.png', 'id' : 1 },
+	'salt' : { 'name' : 'Salt', 'sprite' : 'res://assets/items/silver.png', 'id' : 2 },
+	'sulfur' : { 'name' : 'Sulfur', 'sprite' : 'res://assets/items/silver.png', 'id' : 3 },
+	'quicksilver' : { 'name' : 'Quicksilver', 'sprite' : 'res://assets/items/silver.png', 'id' : 4 },
+	'water' : { 'name' : 'Water', 'sprite' : 'res://assets/items/silver.png', 'id' : 5 },
+	'fire' : { 'name' : 'Fire', 'sprite' : 'res://assets/items/silver.png', 'id' : 6 },
+	'air' : { 'name' : 'Air', 'sprite' : 'res://assets/items/silver.png', 'id' : 7 },
+	'earth' : { 'name' : 'Earth', 'sprite' : 'res://assets/items/silver.png', 'id' : 8 },
+	'base_metal' : { 'name' : 'Base Metal', 'sprite' : 'res://assets/items/silver.png', 'id' : 9 },
 }
