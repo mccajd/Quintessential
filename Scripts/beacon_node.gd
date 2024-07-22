4c9d7f85c9e478ff809c3f9d40ed2864c1ab5510
Jacob McCann <mccajd@gmail.com>
2024-07-21 21:54:42 -0400

    clean up beacon_node

--- Scripts/beacon_node.gd
+++ Scripts/beacon_node.gd
@@ -4,7 +4,8 @@ class_name BeaconNode
 
 @export var id: int
 @export var availableNodeIds: Array[int]
-# Four output slots = array of size 4.  Might as well weaponize this ref type shite
+# Four output slots = array of size 4.
+# This lets me cheat by using the slot_id as an index pointer
 var selected_destination_nodes = [-1, -1, -1, -1]
 
 var hovered = false
@@ -13,7 +14,6 @@ var hovered = false
 @export var inputs: Array[String]
 var outputs
 @export var selected_transformation: String = "neutral"
-#var slottedItem: Item
 
 var products: Array[OutputItem]
 
@@ -22,43 +22,32 @@ var transformer: ItemTransformer
 signal selected
 signal transformed
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	input_pickable = true
 	transformer = ItemTransformer.new()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_handle_button_logic()
-	
-	if (inputs.any(func(item): !!item)):
-		$AnimatedSprite2D.play("filled")
-		return
-	$AnimatedSprite2D.play("empty")
 	
 	var new_outputs = transformer.transform(selected_transformation, inputs)
 	if !Helpers.compare_arrays(outputs, new_outputs):
 		outputs = new_outputs
 		_set_products()
+	
+	if (products.size() > 0):
+		$AnimatedSprite2D.play("filled")
+		return
+	$AnimatedSprite2D.play("empty")
 
-#func add_item(item: Item):
-	#if slottedItem == null:
-		#slottedItem = item
-		#$SlottedItemPreview.texture = item.sprite
-		#return
-#
-#func remove_item():
-	#if slottedItem:
-		#slottedItem = null
-		#$SlottedItemPreview.texture = null
-		#return
 
 func set_transformation(type: String):
 	selected_transformation = type
 
+
 func set_destination_node(slot_id, destination_node_id):
 	selected_destination_nodes[slot_id] = destination_node_id
+
 
 func get_active_connections():
 	var rtn = []
@@ -70,20 +59,23 @@ func get_active_connections():
 		rtn.push_front(destination)
 	return rtn
 
+
 func _handle_button_logic():
 	if (!hovered): return
 
 	if (Input.is_action_just_pressed("select")):
 		selected.emit(self)
 
+
 func _on_mouse_entered():
 	hovered = true
+
 
 func _on_mouse_exited():
 	hovered = false
 
+
 func _set_products():
-	# potential UX later to preserve the destination node.
 	products.clear()
 	if outputs != null:
 		for item in outputs:
