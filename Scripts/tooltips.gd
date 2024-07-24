b42cf98b2bf6878f8c4c7b5ae39a883ce7f2e33d
Rural Young <ruralyoungiii@gmail.com>
2024-07-24 00:30:42 -0400

    Created a parent Control node that *may* be redundent, will revisit later

--- Scripts/tooltips.gd
+++ Scripts/tooltips.gd
@@ -1,11 +1,15 @@
 class_name Tooltips
 extends Node
 
+# NOTE I created a parent Control node for the tool toop, the superparent "Tooltips"
+# May not be necessary, might have to just switch this script and tack it onto "TooltipControlNode"
 @onready var parent = get_parent()
+@onready var textureNode = get_node("TooltipControlNode/TextureRect")
+@onready var textNode = get_node("TooltipControlNode/RichTextLabel")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$TextureRect.texture = load("res://assets/tooltip/dialoguebox.png")
+	textureNode.texture = load("res://assets/tooltip/dialoguebox.png")
 	set_tooltip_message("This is the test of the dynamic box")
 	parent.mouse_entered.connect(_on_mouse_entered)
 	parent.mouse_exited.connect(_on_mouse_exited)
@@ -16,22 +20,24 @@ func _ready():
 func _process(delta):
 	pass
 
-func set_tooltip_message(text):
-	$RichTextLabel.set_text(text)
-	var stringSize = $RichTextLabel.get_combined_minimum_size()
-	$TextureRect.size.x = stringSize.x
-	$TextureRect.size.y = stringSize.y
-
 func _show_tooltip():
-	$TextureRect.show()
-	$RichTextLabel.show()
+	$TooltipControlNode.show()
 
 func _hide_tooltip():
-	$TextureRect.hide()
-	$RichTextLabel.hide()
+	$TooltipControlNode.hide()
 
 func _on_mouse_entered():
 	_show_tooltip()
 
 func _on_mouse_exited():
 	_hide_tooltip()
+
+func set_tooltip_message(text):
+	textNode.set_text(text)
+	var stringSize = textNode.get_combined_minimum_size()
+	textureNode.size.x = stringSize.x
+	textureNode.size.y = stringSize.y
+
+func get_screen_pos():
+	return parent.get_global_transform_with_canvas().origin
+	
