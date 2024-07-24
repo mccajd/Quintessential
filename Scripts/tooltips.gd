bc11916f85a7b293726d4af30d3a3d3de4b5f65a
Rural Young <ruralyoungiii@gmail.com>
2024-07-24 19:41:22 -0400

    Voila, it follows the mouse now

--- Scripts/tooltips.gd
+++ Scripts/tooltips.gd
@@ -18,7 +18,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if $TooltipControlNode.visible:
+		$TooltipControlNode.position = _get_mouse_position()
 
 func _show_tooltip():
 	$TooltipControlNode.show()
@@ -32,12 +33,11 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	_hide_tooltip()
 
+func _get_mouse_position():
+	return get_viewport().get_mouse_position()
+
 func set_tooltip_message(text):
 	textNode.set_text(text)
 	var stringSize = textNode.get_combined_minimum_size()
 	textureNode.size.x = stringSize.x
 	textureNode.size.y = stringSize.y
-
-func get_screen_pos():
-	return parent.get_global_transform_with_canvas().origin
-	
