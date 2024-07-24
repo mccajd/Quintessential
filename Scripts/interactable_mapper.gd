b0e50baef2fcb76a7af2c2ed1ef4bd3df45f8300
Harrison Wine <harrisonmwine@protonmail.com>
2024-07-24 19:33:29 -0400

    replace tile marker with interactable obj

--- Scripts/interactable_mapper.gd
+++ Scripts/interactable_mapper.gd
@@ -8,10 +8,12 @@ var interactable_scene = preload("res://Scenes/interactable.tscn")
 var half_tile := self.tile_set.tile_size*0.5
 
 func _ready():
-	print("Tile map ready!")
 	replace_tiles()
 	
-	
+#replace tile with packaged scene that the player can interact with
 func replace_tiles():
-	for tile in get_used_cells(LAYERS.INTERACTABLES):
-		
+	for tile_pos in get_used_cells(LAYERS.INTERACTABLES):
+		var new_scene : Interactable = interactable_scene.instantiate()
+		new_scene.update_position(map_to_local(tile_pos))
+		add_child(new_scene)
+		erase_cell(LAYERS.INTERACTABLES, tile_pos)
