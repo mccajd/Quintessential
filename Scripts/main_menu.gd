8bdc77ccdfbfe1d5cbbe720b9752aa5903cce8d2
Harrison Wine <harrisonmwine@protonmail.com>
2024-07-28 15:01:22 -0400

    add script to handle main menu inputs

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var game = "res://Levels/beacon_puzzle_main.tscn"
+
+func _on_start_button_pressed():
+	Controller.goto_scene(game)
+
+
+func _on_how_to_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
