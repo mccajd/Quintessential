51944e45595c0d150eace9ce709479fa6b0a2c14
Harrison Wine <harrisonmwine@protonmail.com>
2024-07-28 21:35:13 -0400

    add placeholder howtoscene

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,13 +2,14 @@ extends Node
 
 var game = "res://Levels/beacon_puzzle_main.tscn"
 var credits = "res://Scenes/credits.tscn"
+var howto = "res://Scenes/howto.tscn"
 
 func _on_start_button_pressed():
 	Controller.goto_scene(game)
 
 
 func _on_how_to_button_pressed():
-	pass # Replace with function body.
+	Controller.goto_scene(howto)
 
 
 func _on_credits_button_pressed():
