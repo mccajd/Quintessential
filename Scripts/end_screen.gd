5b3820ebd204fbdc58c569a57af2525bd46357b0
Jacob McCann <mccajd@gmail.com>
2024-07-29 21:42:11 -0400

    WIN

--- Scripts/end_screen.gd
+++ Scripts/end_screen.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var win: bool
+
 func _ready():
 	$Quote.visible = false
 	$AnimatedSprite2D.play("default")
@@ -8,5 +10,14 @@ func _ready():
 func _on_animation_finished():
 	$AnimatedSprite2D.visible = false
 	$Quote.visible = true
-	await get_tree().create_timer(8).timeout	
+	await get_tree().create_timer(5 if win else 8).timeout
+	if win:
+		_fade_quote()
+		return
+	Controller.goto_scene(MainMenu.credits)
+
+func _fade_quote():
+	for i in range(0, 8):
+		$Quote.modulate.a -= 0.125
+		await get_tree().create_timer(0.125).timeout
 	Controller.goto_scene(MainMenu.credits)
